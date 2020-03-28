350596f09f24d7bd1c786e332657121338fa24db
Codeup <Codeup@codeups-mbp.lan>
2020-03-28 18:04:54 -0500

    updated

--- src/main/java/com/matthew/practice/models/Users.java
+++ src/main/java/com/matthew/practice/models/Users.java
@@ -10,9 +10,18 @@ import javax.persistence.Table;
 public class Users {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(nullable = false)
+@Column(nullable = false, unique = true)
 private long id;
-
+@Column(nullable = false, name = "user_role")
+private String userRole;
+@Column(nullable = false)
+private String username;
+@Column(nullable = false)
+private String password;
+@Column(nullable = false, unique = true)
+private String email;
+@Column(nullable = false, columnDefinition = "VARCHAR(20)", name = "phone_number")
+private String phoneNumber;
 
 
 
