52112dfa8209be8ade3c3c6fb29453b9114a1a43
Codeup <Codeup@codeups-mbp.lan>
2020-03-30 06:45:50 -0500

    updated

---
+++ src/main/java/com/matthew/practice/models/DogHasBreed.java
@@ -0,0 +1,10 @@
+package com.matthew.practice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dog_post_has_breed")
+public class DogHasBreed {
+
+}
--- src/main/java/com/matthew/practice/models/Users.java
+++ src/main/java/com/matthew/practice/models/Users.java
@@ -1,7 +1,6 @@
 package com.matthew.practice.models;
 import javax.persistence.*;
 import java.util.List;
-
 @Entity
 @Table(name = "users")
 public class Users {
